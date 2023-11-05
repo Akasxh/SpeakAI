5dc0286e2e6fce087ced67b2972bd0d9a5c0d8b2
Akasxh <115539225+Akasxh@users.noreply.github.com>
2023-11-05 17:24:56 +0530

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,10 +1,10 @@
 role_list = ["Learn", "Talk", "Test"]
 role_templates = {
-    "Learn": """You are an interviewer taking interview of a student for {domain}. Create five questions each in a new line to test the knowledge, communication skills and confidence of the student.
+    "Learn": """You are an tutor taking questions of a student for {domain} . Create five questions each in a new line to test the knowledge of the student.
     Questions:""",
-    "Talk": """You are my distant relative who does not meet me very often. Talk to me about diverse things and ask me questions to test my speaking skills in an informal talk like this. Create five questions each in a new line testing my informal communication skills.
+    "Talk": """You are my distant close friend who does not meet me very often. Talk to me about diverse things and ask me questions to test my general knowledge in an informal talk like this. Create five questions each in a new line testing my knowledge in studies.
     Questions:""",
-    "Test": """Create 5  each in a new line to describe general and easy things like sleep, Effect of fast food, etc, to assess my knowledge.
+    "Test": """Create 5  each in a new line to describe general and easy things like sleep, Effect of fast food, phenomena like reflection, do you think you are dumb , and stuff about solar system, to assess my personality.
     Questions:""",
 }
 
@@ -12,7 +12,7 @@ role_templates = {
 answer_rating_template = """{situation}
 Question {question}
 Answer: {answer}
-Rate communication and child's answer on a scale of 10 in terms of Creativity, Effectiveness and Clarity. Output as a json containing keys "creativity", "effectiveness", "clarity".
+Rate child's answer on a scale of 10 in terms of Creativity, Effectiveness and Clarity. Output as a json containing keys "creativity", "effectiveness", "clarity".
 JSON="""
 
 suggestion_template = """{situation}
